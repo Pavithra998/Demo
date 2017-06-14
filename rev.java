5bd38900e5817403391bf7b981be2da576508204
Pavithra998 <pavierode22@gmail.com>
2017-06-14 09:59:06 +0530

    Create rev.java

--- rev.java
+++ rev.java
@@ -1,1 +1,13 @@
-
+import java.io.*;
+import java.util.*;
+class rev
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    String s1;
+    s1=s.nextInt();
+    s2=s1.reverse();
+    System.out.println(s2);
+  }
+}
