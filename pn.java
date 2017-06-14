7dd11c6caf3ebcf17fc490f19ed2d8f6b93943f5
Pavithra998 <pavierode22@gmail.com>
2017-06-14 09:22:36 +0530

    Create pn.java

---
+++ pn.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class pn
+{
+public static void main(String args[])
+{
+Scanner s=new ScannerSystem.in);
+int n;
+n=s.nextInt();
+{
+if(n<=0)
+System.out.println(n+"Negative");
+}
+else
+{
+if(n>=0)
+System.out.println(n+"Postive");
+}
+else
+System.out.println("zero");
+}
+}
