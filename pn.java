e82b8fb118c7d4fe92624ad0e47358dcd1ae016a
Pavithra998 <pavierode22@gmail.com>
2017-06-14 09:30:20 +0530

    Create pn.java

--- pn.java
+++ pn.java
@@ -4,18 +4,14 @@ class pn
 {
 public static void main(String args[])
 {
-Scanner s=new ScannerSystem.in);
+Scanner s=new Scanner(System.in);
 int n;
 n=s.nextInt();
-{
-if(n<=0)
+if(n<0)
 System.out.println(n+"Negative");
-}
 else
-{
-if(n>=0)
+if(n>0)
 System.out.println(n+"Postive");
-}
 else
 System.out.println("zero");
 }
