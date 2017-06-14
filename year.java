ea2392b1aaad041063c04b8f496b82c22ad3a7c9
Pavithra998 <pavierode22@gmail.com>
2017-06-14 18:26:29 +0530

    Create year.java

---
+++ year.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class year
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n;
+n=s.nextInt();
+if(n%4==0)
+{
+System.out.println("LEAF");
+}
+else
+if(n%4!=0)
+{
+System.out.println(("NOT LEAF");
+}
+}
